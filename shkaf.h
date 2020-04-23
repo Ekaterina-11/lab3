379f01a1975840685637cb13911938f5747ddbdc
Ekaterina-11 <62566219+Ekaterina-11@users.noreply.github.com>
2020-04-23 13:30:31 +0300

    Update shkaf.h

--- shkaf.h
+++ shkaf.h
@@ -4,16 +4,16 @@
 #include "mebel.h"
 
 class shkaf: public mebel{
-    int polki;
+    int shelf;
 public:
     shkaf();
-    shkaf(string name_, int size_, string color_, string material_, int polki_);
+    shkaf(string name_, int size_, string color_, string material_, int shelf_);
     shkaf(const shkaf &p);
 
-    bool SetPolki(int polki_);
+    bool SetPolki(int shelf_);
     int GetPolki() const;
 
     void print() const;
 };
 
-#endif // SHKAF_H_INCLUDED
+#endif
